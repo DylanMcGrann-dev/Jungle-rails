042eabc8bd71ed4556293294f2ce41d1e6f150b9
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-06-24 15:29:17 -0600

    if cart is empty user is prompted with 'your cart is empty' followed by a home link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,10 @@ class ApplicationController < ActionController::Base
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
   helper_method :cart
-
+  
   def enhanced_cart
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+    p @enhanced_cart
   end
   helper_method :enhanced_cart
 
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,9 @@
 class CartsController < ApplicationController
 
   def show
+    
   end
-
+  
   def add_item
     product_id = params[:product_id].to_s
     modify_cart_delta(product_id, +1)
@@ -23,6 +24,7 @@ class CartsController < ApplicationController
     cart[product_id] = (cart[product_id] || 0) + delta
     cart.delete(product_id) if cart[product_id] < 1
     update_cart cart
+    
   end
 
 end

b07b7e7d8c163f8de099ec3b771778c788cc1f98
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-06-26 13:56:54 -0600

    dashboard displays amount of categories and products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME_KEY'], password: ENV['PASSWORD_KEY']
   include HttpAuthConcern
   def show
+    @categoryCount = Category.count
+    @productsCount = Product.count
   end
   
 end

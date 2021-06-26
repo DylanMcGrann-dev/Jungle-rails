086074b2d1abd8b54d50001333d42f31340a829f
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-06-25 20:07:48 -0600

    new page and route for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,6 +3,16 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
   def index
-    @category = Category.order(id: :desc).all
+    @category = Category.all
+    puts @category.name
+  end
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :new
+    end
   end
 end

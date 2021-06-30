5046bd9d6ba7985394c4a77e33aaae9da7c6d59a
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-06-29 19:45:50 -0600

    working on authenticate class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,15 @@ class User < ActiveRecord::Base
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 3 }
+  def self.authenticate_with_credentials(email,password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else 
+      nil
+    end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,5 +28,64 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages[0]).to eq("Password confirmation can't be blank")
     end
 
+    it "should fail when password and confirmation don't match" do
+      @user.password = "notright"
+      expect(@user.save).to be false
+      expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
+    end
+
+    it "should not allow an email that already exists, not case sensitive" do
+      @user.save
+      @user2 = User.new(
+        firstname: "joe",
+        lastname: "dirt",
+        email: "X@y.com",
+        password: "king123",
+        password_confirmation: "king123"
+      )
+      expect(@user2.save).to be false
+      expect(@user2.errors.full_messages[0]).to eq("Email has already been taken")
+    end
+
+    it "should return false when no name is given" do
+      @user.firstname = nil
+      expect(@user.save).to be false
+      expect(@user.errors.full_messages[0]).to eq("Firstname can't be blank")
+    end
+
+    it "should return false when no lastname is given" do
+      @user.lastname = nil
+      expect(@user.save).to be false
+      expect(@user.errors.full_messages[0]).to eq("Lastname can't be blank")
+    end
+
+    it "shouuld return fasle when no email is given" do 
+      @user.email = nil
+      expect(@user.save).to be false
+      expect(@user.errors.full_messages[0]).to eq("Email can't be blank")
+    end
+
+    it "should return false when password is not longer than three characters" do
+      @user.password = "hi"
+      @user.password_confirmation = "hi"
+      expect(@user.save).to be false
+      expect(@user.errors.full_messages[0]).to eq("Password is too short (minimum is 3 characters)")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    before :each do
+      User.create(
+        firstname: "dylan",
+        lastname: "mcgrann",
+        email: "x@y.com",
+        password: "king123",
+        password_confirmation: "king123"
+      )
+    end
+    it "should return true when email and password match" do
+      user = User.authenticate_with_credentials("x@y.com", "king123")
+      expect(user).to be_valid
+    end
   end
 end
